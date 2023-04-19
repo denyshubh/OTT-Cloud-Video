63373d7faf2be4eac6bc9da56fb684707067e660
Shubham Kumar Singh <denyshubham@gmail.com>
2023-04-19 23:24:32 +0000

    initial project setup

---
+++ client-streaming-app/src/App.js
@@ -0,0 +1,12 @@
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App({ signOut, user }) {
+  return (
+    <>
+      <h1>Welcome to my OTT Platform</h1>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
